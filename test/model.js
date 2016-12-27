f3b43fdbd152685e065973305f87497b3cfb6dcb
Redstone Zhao <13740080@qq.com>
2016-12-27 14:05:15 +0000

    test/model.js eslint passed

--- test/model.js
+++ test/model.js
@@ -1,30 +1,29 @@
-var assert  = require('assert')
-  , _       = require('lodash')
-  , utils   = require('../lib/utils')
-  , db      = require('./app/configs').db
-  , model   = require('../lib/model');
+const assert = require('assert');
+const _ = require('lodash');
+const utils = require('../lib/utils');
+const { db } = require('./app/configs');
+const model = require('../lib/model');
 
-describe('lib/model', function() {
+describe('lib/model', () => {
+  describe('#init', () => {
+    const errorLog = utils.logger.error;
+    utils.logger.error = () => {};
 
-  describe('#init', function() {
-    var errorLog = utils.logger.error;
-    utils.logger.error = function() {};
-
-    it('model dir non-exists', function(done) {
-      model.init(db, __dirname + '/models-non-exists', true);
+    it('model dir non-exists', (done) => {
+      model.init(db, `${__dirname}/models-non-exists`, true);
       assert.ok(true);
 
       done();
     });
 
-    it('model dir exists', function(done) {
-      model.init(db, __dirname + '/models');
+    it('model dir exists', (done) => {
+      model.init(db, `${__dirname}/models`);
 
       assert.ok(true);
       done();
     });
 
-    it('check model', function(done) {
+    it('check model', (done) => {
       assert.ok(model('user'));
       assert.ok(model('team'));
       assert.ok(model('book'));
@@ -34,30 +33,30 @@ describe('lib/model', function() {
       done();
     });
 
-    it('table-sync ENV is development', function(done) {
-      var NODE_ENV = process.env.NODE_ENV;
-      var infoLog = utils.logger.info;
+    it('table-sync ENV is development', (done) => {
+      const NODE_ENV = process.env.NODE_ENV;
+      const infoLog = utils.logger.info;
 
       process.env.NODE_ENV = 'development';
 
-      _.each(model(), function(Model) {
-        Model.sync = function() {
-          return new Promise(function(resolve, reject) {
-            setTimeout(function() {
+      _.each(model(), (Model) => {
+        Model.sync = () => (
+          new Promise((resolve) => {
+            setTimeout(() => {
               resolve(Model.name);
             }, 10);
-          });
-        };
+          })
+        );
       });
 
-      utils.logger.info = function(synced, table) {
+      utils.logger.info = (synced, table) => {
         assert.equal('Synced', synced);
         assert.ok(_.includes(['book', 'team', 'user'], table));
       };
 
       process.argv.push('table-sync');
 
-      model.init(db, __dirname + '/models');
+      model.init(db, `${__dirname}/models`);
 
       utils.logger.info = infoLog;
       process.env.NODE_ENV = NODE_ENV;
@@ -65,20 +64,17 @@ describe('lib/model', function() {
       done();
     });
 
-    it('table-sync ENV is development, exclude table-sync in argv', function(done) {
-      var NODE_ENV = process.env.NODE_ENV;
-      var infoLog = utils.logger.info;
+    it('table-sync ENV is development, exclude table-sync in argv', (done) => {
+      const NODE_ENV = process.env.NODE_ENV;
+      const infoLog = utils.logger.info;
 
       process.env.NODE_ENV = 'development';
-
-      model.init(db, __dirname + '/models');
+      model.init(db, `${__dirname}/models`);
 
       utils.logger.error = errorLog;
       utils.logger.info = infoLog;
       process.env.NODE_ENV = NODE_ENV;
       done();
     });
-
   });
-
 });
